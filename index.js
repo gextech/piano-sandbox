91889e6e2af3050fc781e1b044f999fa6cbf2150
Isain Hernandez <isain.hernandezb@gmail.com>
2017-01-26 16:42:02 -0600

    added private dir

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var app = express();
 
 app.use(express.static('public'))
 
+app.use('/private', express.static('public/private'))
+
 app.get('/api', function(req, res){
   console.log(req.headers);
   res.send(req.headers);
 });
+
+
 
 app.listen(3000, function(){
   console.log("Running server on 3000");
---
+++ public/js/piano.js
@@ -0,0 +1,13 @@
+tp = window["tp"] || [];
+tp.push(["setAid", "Gw0qLKfjdV"]);
+tp.push(["setDebug", true]);
+tp.push(["setSandbox", true]);
+tp.push(["setEndpoint", "https://sandbox.tinypass.com"]);
+(function(src) {
+    var a = document.createElement("script");
+    a.type = "text/javascript";
+    a.async = true;
+    a.src = src;
+    var b = document.getElementsByTagName("script")[0];
+    b.parentNode.insertBefore(a, b)
+})("https://sandbox.tinypass.com/xbuilder/experience/load?aid=Gw0qLKfjdV");
