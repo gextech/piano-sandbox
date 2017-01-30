97a4a1d5a8f2eb08b835119210af8d9ce2fcb9f7
atomsfat <atomsmail@gmail.com>
2017-01-30 11:00:10 -0600

    update a lot of things

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
 
+tinypass = require('tinypass').createClient({
+  aid: process.env.TINYPASS_APPLICATION_ID,
+  privateKey: process.env.TINYPASS_PRIVATE_KEY
+});
+
+console.log("appID", process.env.TINYPASS_APPLICATION_ID);
+var eventHandlers = require('./event-handlers');
+
 app.use(express.static('html'));
 app.use(cookieParser());
 
 app.use('/private', express.static('html/private'))
 app.use('/restricted', express.static('html/restricted'))
 
-app.get('/api', function(req, res){
+app.get('/api', function (req, res) {
   console.log(req.headers);
   res.send(req.headers);
 });
 
 
-
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Running server on 3000");
 });
 
@@ -30,4 +37,36 @@ app.post('/login', function (req, res) {
 app.post('/logout', function (req, res) {
   clearCookie('logged');
   res.send('cookie foo cleared');
+});
+
+
+app.get('/webhook', function(req, res) {
+  try {
+    var data = req.query.data;
+
+  // Decrypt the data into a payload String object
+  var payload = tinypass.decrypt(data);
+
+  // Turn the string into a Buffer object
+  payload = Buffer.from(payload);
+
+  // Filter the Buffer elements to standard ASCII so we can use JSON.parse
+  // payload = payload.filter(char = > {
+  //     return char > 31;
+  // });
+
+  // Finally parse the String into JSON
+  payload = JSON.parse(payload);
+
+  if (eventHandlers[payload.type] && eventHandlers[payload.type][payload.event] && typeof eventHandlers[payload.type][payload.event] === 'function') {
+    eventHandlers[payload.type][payload.event].call(undefined, payload);
+  } else {
+    console.warn(payload.type + " " +  payload.event + " no implemented.", payload);
+  }
+
+  res.send({message: 'ok'});
+  } catch (err) {
+    console.error(err.stack);
+    res.status(500).send({message: err.message});
+  }
 });
