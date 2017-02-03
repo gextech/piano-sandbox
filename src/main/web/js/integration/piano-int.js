31758f14c100203c5c6a80150f3fcaa6d3089119
atomsfat <atomsmail@gmail.com>
2017-02-02 18:02:00 -0600

    Basic angular integration

--- src/main/web/js/integration/piano-int.js
+++ src/main/web/js/integration/piano-int.js
@@ -1,8 +1,29 @@
 console.log("here ------->>>", this);
 console.log("here ------->>>", angular);
 
-console.log("scope", $scope);
 
-function hola() {
-  console.log("------------>>>>>> HOLAß");
-}
+
+var scope = angular.element($("#thisForm")).scope();
+
+
+scope.$apply(function() {
+  scope.double = function(value) { return value * 2; };
+  scope.isValid = false;
+  scope.customVar = "This is customVar"
+  scope.helloWorld  = function (str) {
+    console.log("helloWorld" + str);
+    return "HOLA " + str;
+  }
+
+  scope.validateUser  = function (str) {
+    console.log("validate" + str)
+    if(str === undefined || str === ""){
+      scope.isValid = false;
+    } else{
+      scope.isValid = true;
+    }
+
+
+    return scope.isValid;
+  }
+});
