d93be442c2a25cf553ec2f4b612a457a4cf9429b
Juan Mendivil <juan@mendivil.mx>
2017-02-10 21:07:52 -0600

    generate pretty html with jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
 /* eslint-disable */
 var jade = require('gulp-jade');
 var gulp = require('gulp');
-var cors = require('cors');
 var nodemon = require('gulp-nodemon');
 
 gulp.task('templates', function() {
-  var YOUR_LOCALS = {};
+  gulp.src(['src/web/**/*.js'])
+    .pipe(gulp.dest('public'));
 
-  gulp.src(['./src/web/**/*.js'])
-    .pipe(gulp.dest('./public'));
-
-  gulp.src(['./src/web/**/*.jade'])
-    .pipe(jade({
-      locals: YOUR_LOCALS
-    }))
-    .pipe(gulp.dest('./public'))
+  gulp.src(['src/web/**/*.jade', '!src/web/header.jade'])
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest('public'))
 });
 
 gulp.task('nodemon', function runNodemon(cb) {
   var started = false;
   return nodemon({
-    script: './src/server/index.js',
+    script: '/src/server/index.js',
     tasks : ['templates'],
     ignore: ['/public/**'],
     watch : ['/src/'],
